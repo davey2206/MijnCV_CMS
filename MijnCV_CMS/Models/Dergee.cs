9a087cb6a23f16c29b80688c0ff5a36893b71ac1
davey2206 <davey2206@live.nl>
2022-09-19 11:37:05 +0200

    added models

---
+++ MijnCV_CMS/Models/Dergee.cs
@@ -0,0 +1,13 @@
+﻿namespace MijnCV_CMS.Models
+{
+    public class Dergee
+    {
+        public int ID { get; set; }
+        public string CV { get; set; }
+        public string Title { get; set; }
+        public string Paragraph { get; set; }
+        public string Image { get; set; }
+        public int Layout { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ MijnCV_CMS/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MijnCV_CMS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string CV { get; set; }
+        public string Email { get; set; }
+    }
+}
