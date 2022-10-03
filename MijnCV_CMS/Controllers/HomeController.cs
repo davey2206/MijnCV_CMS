f30d66416b2ec88d5d862827bf43ef11ea3b92d9
davey2206 <davey2206@live.nl>
2022-10-03 11:30:24 +0200

    added post and get actions for sections

--- MijnCV_CMS/Controllers/HomeController.cs
+++ MijnCV_CMS/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MijnCV_CMS.Models;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace MijnCV_CMS.Controllers
 {
@@ -13,13 +15,40 @@ namespace MijnCV_CMS.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            List<Section> sections = new List<Section>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7059/api/Sections"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    sections = JsonConvert.DeserializeObject<List<Section>>(apiResponse);
+                }
+            }
+            ViewData["Sections"] = sections;
             return View();
         }
+
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([Bind("CV,Title,Paragraph,Image,Layout,Position,PageID")] Section section)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(section), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:7059/api/Sections", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- MijnCV_CMS/Models/Section.cs
+++ MijnCV_CMS/Models/Section.cs
@@ -1,14 +1,18 @@
-﻿namespace MijnCV_CMS.Models
+﻿using Newtonsoft.Json;
+
+namespace MijnCV_CMS.Models
 {
     public class Section
     {
+        [Newtonsoft.Json.JsonProperty("Id", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public int ID { get; set; }
+
         public string CV { get; set; }
         public string? Title { get; set; }
         public string? Paragraph { get; set; }
         public string? Image { get; set; }
         public int Layout { get; set; }
         public int Position { get; set; }
-        public Page Page { get; set; }
+        public int PageID { get; set; }
     }
 }
