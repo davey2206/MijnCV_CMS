dd2ecb799e29b912750c2b2d697a853ba9bd98b2
davey2206 <davey2206@live.nl>
2022-11-30 12:04:11 +0100

    added google login

--- MijnCV_CMS/Controllers/HomeController.cs
+++ MijnCV_CMS/Controllers/HomeController.cs
@@ -1,87 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using MijnCV_CMS.Models;
 using Newtonsoft.Json;
-using System.Diagnostics;
-using System.Text;
 
-namespace MijnCV_CMS.Controllers
+namespace MijnCV_CMS.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public async Task<IActionResult> Index()
+    {
+        List<Section> sections = new List<Section>();
+        List<Page> pages = new List<Page>();
+        using (var httpClient = new HttpClient())
         {
-            _logger = logger;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            List<Section> sections = new List<Section>();
-            List<Page> pages = new List<Page>();
-            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync("https://localhost:7059/api/Sections"))
             {
-                using (var response = await httpClient.GetAsync("https://localhost:7059/api/Sections"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    sections = JsonConvert.DeserializeObject<List<Section>>(apiResponse);
-                }
-
-                using (var response = await httpClient.GetAsync("https://localhost:7059/api/Pages"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    pages = JsonConvert.DeserializeObject<List<Page>>(apiResponse);
-                }
-            }
-            ViewData["Sections"] = sections;
-            ViewData["Pages"] = pages;
-            return View();
-        }
-
-        public async Task<IActionResult> Add()
-        {
-            List<Page> pages = new List<Page>();
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("https://localhost:7059/api/Pages"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    pages = JsonConvert.DeserializeObject<List<Page>>(apiResponse);
-                }
-            }
-            ViewData["Pages"] = pages;
-
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddAsync([Bind("CV,Title,Paragraph,Image,Layout,Position,PageID")] Section section)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                StringContent content = new StringContent(JsonConvert.SerializeObject(section), Encoding.UTF8, "application/json");
-
-                using (var response = await httpClient.PostAsync("https://localhost:7059/api/Sections", content))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                }
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                sections = JsonConvert.DeserializeObject<List<Section>>(apiResponse);
             }
 
-            return RedirectToAction("Index", "Home");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteAsync(string Id)
-        {
-            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync("https://localhost:7059/api/Pages"))
             {
-                using (var response = await httpClient.DeleteAsync("https://localhost:7059/api/Sections/" + Id))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                }
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                pages = JsonConvert.DeserializeObject<List<Page>>(apiResponse);
             }
-
-            return RedirectToAction("Index", "Home");
         }
+        ViewData["Sections"] = sections;
+        ViewData["Pages"] = pages;
+        return View();
+    }
+
+    public async Task<IActionResult> Add()
+    {
+        List<Page> pages = new List<Page>();
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync("https://localhost:7059/api/Pages"))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                pages = JsonConvert.DeserializeObject<List<Page>>(apiResponse);
+            }
+        }
+        ViewData["Pages"] = pages;
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync([Bind("CV,Title,Paragraph,Image,Layout,Position,PageID")] Section section)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(section), Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PostAsync("https://localhost:7059/api/Sections", content))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteAsync(string Id)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.DeleteAsync("https://localhost:7059/api/Sections/" + Id))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ MijnCV_CMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MijnCV_CMS.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
